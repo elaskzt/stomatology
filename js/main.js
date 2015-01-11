da0d90d6d4555fc1ee11e9c93a0bf455ccfe1939
AlbertRAR <behappyhappybee@gmail.com>
2015-01-11 22:40:54 +0300

    Created site v.1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	var line = new Vivus('sec-1-svg-path', {type: 'scenario-sync', duration: 100, start: 'autostart'});
+
+	var wow = new WOW({	mobile: false	});
+	wow.init();
+
+ });
+
