9ef36e780e2c425807887d6179af35fabf713262
AlbertRAR <behappyhappybee@gmail.com>
2015-01-12 12:21:26 +0300

    some improvements

--- js/main.js
+++ js/main.js
@@ -21,5 +21,19 @@ $(document).ready(function() {
 		$(this).children('.elas-link').css('fill', '#71B6E9');
 	});
 
+	$('.work-steps').hover(function() {
+		$(this).children('.digit').css('color', '#ef422e');
+		$(this).children('.work-steps-txt').css({
+			background: '#ef422e',
+			color: '#fff'
+		});
+	}, function() {
+		$(this).children('.digit').css('color', '#fff');
+		$(this).children('.work-steps-txt').css({
+			background: '#fff',
+			color: '#24333e'
+		});
+	});
+
  });
 
